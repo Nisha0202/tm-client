d6734a5c028468b9191c54b6ed53bd47bd4932b8
Nisha0202 <nishajabatunnessa@gmail.com>
2024-04-29 06:37:56 +0600

    data added

--- src/components/Spots.jsx
+++ src/components/Spots.jsx
@@ -1,26 +1,28 @@
 import React, { useEffect, useState } from 'react'
 import Card from './Card'
+import { Link } from 'react-router-dom';
 
 export default function Spots() {
 
     const [data, setData] = useState([]);
 
-    useEffect(() => {
-      fetch('spots.json')
-        .then(response => response.json())
-        .then(data => setData(data))
-        .catch(error => console.error('Error:', error));
-    }, []);
+    // useEffect(() => {
+    //   fetch('spots.json')
+    //     .then(response => response.json())
+    //     .then(data => setData(data))
+    //     .catch(error => console.error('Error:', error));
+    // }, []);
   
   return (
     <div className='flex flex-col justify-center items-center pb-12'>
        <h1 className='my-12 text-2xl font-bold'>Tourist Spots</h1>
          <div className='flex flex-wrap flex-col gap-4 md:flex-row md:justify-between md:gap-12'>
 
-{data.map((spots, index) => (
-        <Card key={index} spots={spots} />
-      ))}
+         {data.slice(0, 3).map((spots, index) => (
+    <Card key={index} spots={spots} />
+))}
     </div>
+    <Link to={'/all'} className='my-4 btn btn-link text-base'>See More</Link>
     </div>
    
   )
--- src/pages/AddList.jsx
+++ src/pages/AddList.jsx
@@ -51,8 +51,7 @@ const AddList = () => {
               return;
           }
         console.log('Form data submitted:', formData);
-
-         fetch('http://localhost:5000//touristspots', {
+         fetch('http://localhost:5000/touristspots', {
         method: 'POST',
         headers: {
             'content-type': 'application/json'
