d4342449387a04bccd96344060cf491a59fb4755
Nisha0202 <nishajabatunnessa@gmail.com>
2024-04-30 18:56:10 +0600

    card detail

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useContext } from 'react'
 import { AuthContext } from '../FirebaseProbider/FirbaseProvider';
  import { useNavigate } from 'react-router-dom';
+ import { Tooltip } from 'react-tooltip';
 const Card = ({ spot, spots, setSpots}) => {
 // Inside your component
 const navigate = useNavigate();
@@ -15,13 +16,18 @@ const navigate = useNavigate();
   };
   return (
     <div className='' >
-      <div className="card w-80 bg-base-100 md:w-80 border-2 p-4 rounded-xl h-xs ">
+      <div className="card w-72 bg-base-100 md:w-80 border-2 p-4 rounded-xl h-xs " >
                <img className='w-full h-80 rounded-md border-2' src={spot.image_url} alt={spot.tourists_spot_name} />
       <h2 className='font-bold text-xl text-center my-2'>{spot.tourists_spot_name}</h2>
       <p className='text-center text-lg text-wrap'>{spot.country_name}</p>
       <p className='text-center text-wrap'>Average Cost: {spot.average_cost}£</p>
-      <div className='btn w-full rounded-md text-white bg-teal-800 mt-2' onClick={handleView}>View Details</div>
+      <div><p className='text-center text-wrap py-3 text-sm/relaxed'> {spot.short_description}</p></div>
+      <div className='flex justify-between mb-3'>
+        <p className='text-center text-wrap '> {spot.seasonality}</p>
+        <p className='text-center text-wrap '> {spot.location}</p></div>
+      <div className='btn w-full rounded-md text-white bg-teal-800 mt-2' onClick={handleView} data-tooltip-id="my-tooltip" data-tooltip-content="Click to see more">View Details</div>
     </div>
+    <Tooltip id="my-tooltip" />
     </div>
   );
 }
